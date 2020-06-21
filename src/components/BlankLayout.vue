9d05981afd0f8fde9173243d053a379417d79eea
halitenes <buyuktepe.halitenes@gmail.com>
2020-06-21 03:40:50 +0300

    ne yazayım kardeşim ne yazayım

---
+++ src/components/BlankLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container fill-height fluid >
+    <router-view />
+  </v-container>
+</template>
+<script>
+export default {
+  name: "BlankLayout",
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  },
+};
+</script>
---
+++ src/components/MainLayout.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container fluid class="ma-0 pa-0">
+    <v-navigation-drawer
+      v-model="$store.state.drawer"
+      width="450"
+      class="elevation-6"
+      right
+      app
+    >
+      <v-list>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>Terminal</v-list-item-title>
+          <v-btn absolute right text color="primary" @click="clearTerminal()">Clear</v-btn>
+        </v-list-item-content>
+      </v-list-item>
+        <v-divider/>
+        <v-list-item-content v-for="(array,index) in $store.state.messageArray" :key="index">
+          <v-list-item-title>{{array}}</v-list-item-title>
+          <v-divider/>
+        </v-list-item-content>
+      </v-list>
+    </v-navigation-drawer>
+    <v-content>
+        <router-view />
+    </v-content>
+  </v-container >
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+  created() {
+    console.log("Mainla")
+  },
+  data() {
+    return {
+      drawer: false
+    };
+  },
+  methods:{
+    clearTerminal() {
+      this.$store.state.messageArray = []
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
